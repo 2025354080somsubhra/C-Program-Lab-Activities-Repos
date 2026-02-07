b2cb959729f8889d8fef65abb0d7a1014ced30e4
Somsubhra Chatterjee <2025354080.somsubhra@ug.sharda.ac.in>
2026-02-07 13:55:40 +0530

    Update week5.c

--- week5.c
+++ week5.c
@@ -1,19 +1,19 @@
 // Tower of Hanoi
-// #include<stdio.h>
-// void toh(int n, char src, char aux, char dest) {
-//     if(n > 0) {
-//         toh(n-1,src,dest,aux);
-//         printf("%c --> %c\t",src,dest);
-//         toh(n-1,aux,src,dest);
-//     }
-// }
+#include<stdio.h>
+void toh(int n, char src, char aux, char dest) {
+    if(n > 0) {
+        toh(n-1,src,dest,aux);
+        printf("%c --> %c\t",src,dest);
+        toh(n-1,aux,src,dest);
+    }
+}
 
-// int main() {
-//     int n;
-//     printf("Enter total no.s of discs :");
-//     scanf("%d",&n);
-//     toh(n,'A','B','C');
-// }
+int main() {
+    int n;
+    printf("Enter total no.s of discs :");
+    scanf("%d",&n);
+    toh(n,'A','B','C');
+}
 
 // Enter DOB using Structure
 
@@ -32,4 +32,5 @@ int main() {
     printf("%d/%d/%d\n",dob.dd,dob.mm,dob.yy);
     printf("%d-%s-%d",dob.dd,month[dob.mm-1],dob.yy);
     return 0;
+
 }
